08dbcf4e75e939a516181b752787eb43a2b088b9
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-05-03 23:27:59 +1000

    print ip of suspicious smb source

--- responderpot.py
+++ responderpot.py
@@ -1,6 +1,8 @@
 import argparse
+import datetime
 import random
 import string
+import sys
 import time
 import traceback
 import smb
@@ -10,25 +12,26 @@ def randchars(mn, mx):
     return ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(random.randint(mn, mx)))
 
 def connect(domain, username, password, client, server):
-    conn = SMBConnection(username, password, client, server, domain=domain, use_ntlm_v2=True, is_direct_tcp=True)
-    conn.connect('0.0.0.0', 445)
-    conn.close()
+    try:
+        conn = SMBConnection(username, password, client, server, domain=domain, use_ntlm_v2=True, is_direct_tcp=True)
+        conn.connect('0.0.0.0', 445)
+        conn.close()
+    except smb.smb_structs.ProtocolError as e:
+        ip, port = conn.sock.getpeername()
+        print('{} INFO SUSPICIOUS_SMB_RESPONSE {} {}'.format(str(datetime.datetime.now()), ip, port))
+    except ConnectionRefusedError as e:
+        pass
+    except Exception as e:
+        sys.stderr.write('{} ERROR {}'.format(str(datetime.datetime.now()), traceback.format_exc()))
 
-def main(domain, throttle):
+def monitor(domain, throttle):
     while True:
-        try:
-            connect(domain, randchars(6, 12), randchars(6, 12), randchars(6, 12), randchars(6, 12))
-        except smb.smb_structs.ProtocolError as e:
-            print("ALERT: UNEXPECTED RESPONSE DETECTED")
-        except ConnectionRefusedError as e:
-            pass
-        except Exception as e:
-            print(traceback.format_exc())
+        connect(domain, randchars(6, 12), randchars(6, 12), randchars(6, 12), randchars(6, 12))
         time.sleep(throttle)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--domain', '-d', help='target domain for monitoring')
+    parser.add_argument('--domain', '-d', default='WORKGROUP', help='target domain for monitoring')
     parser.add_argument('--throttle', '-t', type=int, default=10, help='throttle requests in seconds')
     args = parser.parse_args()
-    main(args.domain, args.throttle)
+    monitor(args.domain, args.throttle)
